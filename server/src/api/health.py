61ede1f7dda0b07dd769bf4dd1aff4d08d2110d4
Rudolf Stander <rudolfstan@gmail.com>
2025-06-06 10:28:21 +0200

    feat: add health endpoint

---
+++ server/src/api/health.py
@@ -0,0 +1,39 @@
+from controllers.scaling_manager import ScalingManager
+from fastapi import APIRouter
+
+from library.fastapi_root import FastAPIRoot
+
+###############################################################################
+## API REGISTRATION                                                          ##
+###############################################################################
+
+router = APIRouter(prefix="/", tags=["health"])
+
+
+def register(fastapi_root: FastAPIRoot = None):
+    if fastapi_root is None:
+        FastAPIRoot.instance().register_router(router)
+    else:
+        fastapi_root.register_router(router)
+
+
+###############################################################################
+
+#
+# TODO: add DB, AWS and k8s checks to below
+#
+
+@router.get("/healthz")
+def healthz():
+    return {"status": "ok"}
+
+
+@router.get("/readyz")
+def healthz():
+    return {"status": "ok"}
+
+
+@router.get("/livez")
+def healthz():
+    return {"status": "ok"}
+
