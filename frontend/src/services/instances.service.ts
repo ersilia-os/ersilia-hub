f18f7898da764249fbce8fafc8c310cfbe293ac8
Rudolf Stander <rudolfstan@gmail.com>
2025-11-23 20:03:52 +0200

    wip: modelinstance api + ui v2

--- frontend/src/services/instances.service.ts
+++ frontend/src/services/instances.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, Injectable, Signal, signal, WritableSignal } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { catchError, map, Observable, Subscription, throwError } from 'rxjs';
 import { environment } from '../environments/environment';
-import { ModelInstance, ModelInstanceFilters, ModelInstanceFromApi } from '../objects/instance';
+import { ExtendedModelInstance, ExtendedModelInstanceFromApi, InstanceLogEntry, InstanceLogEntryFromApi, JobLogsFilters, ModelInstance, ModelInstanceFilters, ModelInstanceFromApi } from '../objects/instance';
 import { APIFiltersMap, APIList } from '../objects/common';
+import { mapHttpError } from '../app/utils/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstancesService {
 
-  private instances: WritableSignal<ModelInstance[]> = signal([]);
+  private instances: WritableSignal<ExtendedModelInstance[]> = signal([]);
   private instancesLoading: WritableSignal<boolean> = signal(false);
 
   constructor(private http: HttpClient) { }
@@ -18,9 +19,9 @@ export class InstancesService {
   loadInstances(filters: ModelInstanceFilters): Subscription {
     this.instancesLoading.set(true);
 
-    return this.http.get<APIList<ModelInstance>>(`${environment.apiHost}/api/instances`, { params: APIFiltersMap(filters) })
+    return this.http.get<APIList<ExtendedModelInstance>>(`${environment.apiHost}/api/instances`, { params: APIFiltersMap(filters) })
       .subscribe(instancesList => {
-        let mappedList: ModelInstance[] = instancesList.items.map(ModelInstanceFromApi);
+        let mappedList: ExtendedModelInstance[] = instancesList.items.map(ExtendedModelInstanceFromApi);
         this.instances.set(mappedList);
         this.instancesLoading.set(false);
 
@@ -28,11 +29,51 @@ export class InstancesService {
       });
   }
 
-  getInstancesSignal(): Signal<ModelInstance[]> {
+  loadInstanceJobLogs(filters: JobLogsFilters): Observable<string[] | undefined> {
+    return this.http.get<{ logs: string[] }>(`${environment.apiHost}/api/instances/job-logs`, { params: APIFiltersMap(filters) })
+      .pipe(
+        map((response: { logs: string[] }) => {
+          return response.logs;
+        }),
+        catchError(error => {
+          const errorString = mapHttpError(error);
+          const submissionResult = {
+            success: false,
+            error: errorString
+          };
+
+          // Return an observable with a user-facing error message.
+          return throwError(() => new Error(errorString));
+        })
+      );
+  }
+
+  loadInstanceHistory(filters: ModelInstanceFilters): Observable<InstanceLogEntry[]> {
+    return this.http.get<{ history: InstanceLogEntry[] }>(`${environment.apiHost}/api/instances/history`, { params: APIFiltersMap(filters) })
+      .pipe(
+        map((response: { history: InstanceLogEntry[] }) => {
+          return response.history.map(InstanceLogEntryFromApi)
+        }),
+        catchError(error => {
+          const errorString = mapHttpError(error);
+          const submissionResult = {
+            success: false,
+            error: errorString
+          };
+
+          // Return an observable with a user-facing error message.
+          return throwError(() => new Error(errorString));
+        })
+      );
+  }
+
+  // TODO: [instanes v2] - /actions api
+
+  getInstancesSignal(): Signal<ExtendedModelInstance[]> {
     return computed(() => this.instances());
   }
 
-  computeInstancesSignal<T>(computation: (models: ModelInstance[]) => T): Signal<T> {
+  computeInstancesSignal<T>(computation: (models: ExtendedModelInstance[]) => T): Signal<T> {
     return computed(() => computation(this.instances()));
   }
 
